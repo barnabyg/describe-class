aeb0ee1eb8d2955ae5e43ba6332ad5777023dbd9
barnabyg <barnabyg@blizzardtec.com>
2014-06-02 13:38:47 +0100

    first add

---
+++ src/main/java/makeclass/showclass/ShowClazz.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package makeclass.showclass;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import com.blizzardtec.Vehicle;
+
+/**
+ * @author Barnaby Golden
+ *
+ */
+public final class ShowClazz {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle();
+
+        Method methods[] = vehicle.getClass().getMethods();
+
+        System.out.println("METHODS: ");
+        for (int i = 0; i < methods.length; i++) {
+            System.out.println(methods[i].getName());
+        }
+
+        Field fields[] = vehicle.getClass().getDeclaredFields();
+        System.out.println("FIELDS: ");
+        for (int i = 0; i < fields.length; i++) {
+            System.out.println(fields[i].getName());
+        }
+    }
+}
